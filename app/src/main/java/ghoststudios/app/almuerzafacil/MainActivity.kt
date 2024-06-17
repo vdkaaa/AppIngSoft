f49142f1bca8907c6cbf5eebc0f7d558c6469a79
Diego Santander <102480202+vdkaaa@users.noreply.github.com>
2024-06-17 13:22:12 -0400

    Update2

--- app/src/main/java/ghoststudios/app/almuerzafacil/MainActivity.kt
+++ app/src/main/java/ghoststudios/app/almuerzafacil/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AlmuerzaFacilTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Diego",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
