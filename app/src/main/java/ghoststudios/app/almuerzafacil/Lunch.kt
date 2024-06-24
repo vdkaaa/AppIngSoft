5df7d91328d53ec5f33b36a8c24751a2f6d71e05
Samu385 <samquiroz385@gmail.com>
2024-06-23 21:43:36 -0400

    Subir imagenes a la base de datos

---
+++ app/src/main/java/ghoststudios/app/almuerzafacil/Lunch.kt
@@ -0,0 +1,10 @@
+package ghoststudios.app.almuerzafacil
+
+data class Lunch (
+    val id : String? = null,
+    val name : String? = null,
+    val description:String? = "No description added",
+    val imgUri: String? = null
+)
+
+
--- app/src/main/java/ghoststudios/app/almuerzafacil/TestFirebaseConnection.kt
+++ app/src/main/java/ghoststudios/app/almuerzafacil/TestFirebaseConnection.kt
@@ -1,19 +1,25 @@
 package ghoststudios.app.almuerzafacil
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import ghoststudios.app.almuerzafacil.databinding.ActivityTestFirebaseConnectionBinding
 
 class TestFirebaseConnection : AppCompatActivity() {
 
-    lateinit var binding : ActivityTestFirebaseConnectionBinding
-    lateinit var firebaseRef:DatabaseReference
+    private lateinit var binding : ActivityTestFirebaseConnectionBinding
+    private lateinit var firebaseRef:DatabaseReference
+    private lateinit var storageRef :StorageReference
+    private var uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +30,56 @@ class TestFirebaseConnection : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        firebaseRef =FirebaseDatabase.getInstance().getReference("test")
+
+
+
+        firebaseRef = FirebaseDatabase.getInstance().getReference("test")
+        storageRef = FirebaseStorage.getInstance().getReference("Images")
+
         binding = ActivityTestFirebaseConnectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.sendDataText.setOnClickListener{
-            firebaseRef.setValue("Toma p...").addOnCompleteListener(){
-                Toast.makeText(this, "data stored", Toast.LENGTH_SHORT).show()
+        binding.imgAddTest.setImageResource(R.drawable.tutorial)
+
+        val pickedImage = registerForActivityResult(ActivityResultContracts.GetContent()){
+            binding.imgAddTest.setImageURI(it)
+            if(it != null){
+                uri = it
             }
 
+        }
+        binding.pickImageBtn.setOnClickListener{
+            pickedImage.launch("image/*")
+        }
 
+
+
+        //Send data to the realtime database
+        binding.sendDataText.setOnClickListener{
+
+            val lunchId = firebaseRef.push().key!!
+            //val lunch = Lunch(lunchId,"Pan con pan","Descripción del pan con pan")
+            var lunch : Lunch
+
+            uri?.let {
+                storageRef.child(lunchId).putFile(it)
+                    .addOnSuccessListener { task->
+                        task.metadata!!.reference!!.downloadUrl.addOnSuccessListener { url->
+                            Toast.makeText(this, "image stored", Toast.LENGTH_SHORT).show()
+
+                            val imgUrl = url.toString()
+
+                            lunch = Lunch(lunchId,"Pan con pan","Descripción del pan con pan", imgUrl)
+
+                            firebaseRef.child(lunchId).setValue(lunch).addOnCompleteListener{
+                                Toast.makeText(this, "data stored", Toast.LENGTH_SHORT).show()
+                            }.addOnFailureListener{
+                                Toast.makeText(this, "data failed to store ", Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+                    }
+            }
         }
 
     }
