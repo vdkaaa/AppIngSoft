8a74201add7d2976173f169acb0172ea7ab61149
Samu385 <samquiroz385@gmail.com>
2024-07-01 21:31:44 -0400

    Agendar almuerzos mejorado

--- app/src/main/java/ghoststudios/app/almuerzafacil/ui/theme/LunchAdapterClass.kt
+++ app/src/main/java/ghoststudios/app/almuerzafacil/ui/theme/LunchAdapterClass.kt
@@ -1,6 +1,7 @@
 package ghoststudios.app.almuerzafacil.ui.theme
 
 import android.content.Context
+import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,12 +13,14 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.google.firebase.database.FirebaseDatabase
 import com.squareup.picasso.Picasso
 import ghoststudios.app.almuerzafacil.R
+import java.util.Date
 
 class LunchAdapterClass(private val lunches: ArrayList<Lunch>,
                         private val showMenu:(Boolean) ->Unit ):
     Adapter<LunchAdapterClass.ViewHolderClass>() {
     private var isEnable = false
     private var itemSelectedList = mutableListOf<Int>()
+    private val viewHolders = SparseArray<ViewHolderClass>()
 
     class ViewHolderClass(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var itemImage: ImageView = itemView.findViewById(R.id.CV_imageLunch)
@@ -37,6 +40,7 @@ class LunchAdapterClass(private val lunches: ArrayList<Lunch>,
     }
 
     override fun onBindViewHolder(holder: ViewHolderClass, i: Int) {
+        viewHolders.put(i, holder)
         holder.itemHeader.text  = lunches[i].name
         holder.itemDescription.text = lunches[i].description
         holder.itemPrice.text = lunches[i].price.toString()
@@ -48,7 +52,7 @@ class LunchAdapterClass(private val lunches: ArrayList<Lunch>,
         }
         holder.itemView.setOnClickListener{
             if(itemSelectedList.contains(i)){
-                itemSelectedList.removeAt(i)
+                itemSelectedList.remove(i)
                 holder.itemTick.visibility = View.GONE
                 if(itemSelectedList.isEmpty()){
                     showMenu(false)
@@ -71,13 +75,24 @@ class LunchAdapterClass(private val lunches: ArrayList<Lunch>,
 
         if(itemSelectedList.isNotEmpty()){
             val clientID = "1111"
-            val listOfLunches = ArrayList<Lunch>()
             val firebaseRef = FirebaseDatabase.getInstance().getReference("orders")
             val orderId = firebaseRef.push().key!!
+
+
+            val currentDate = Date()
+            val order = Order(
+                id = orderId,
+                idClient = clientID,
+                dateOrdered = currentDate,
+                dateToDeliver = currentDate,
+                total = 0,
+                wasDelivered = false
+            )
             for (index in itemSelectedList){
-                listOfLunches.add(lunches[index])
+                order.addLunch(lunches[index].id!!, 1)
+                viewHolders[index]?.itemTick?.visibility = View.GONE
             }
-            val order = Order(orderId,clientID,listOfLunches)
+
 
             firebaseRef.child(orderId).setValue(order).addOnCompleteListener {
                 Toast.makeText(cont, "data stored", Toast.LENGTH_SHORT).show()
@@ -85,7 +100,11 @@ class LunchAdapterClass(private val lunches: ArrayList<Lunch>,
                 Toast.makeText(cont, "data failed to store ", Toast.LENGTH_SHORT)
                     .show()
             }
+
         }
+        showMenu(false)
+        isEnable = false
+
     }
 
 }
--- app/src/main/java/ghoststudios/app/almuerzafacil/ui/theme/Order.kt
+++ app/src/main/java/ghoststudios/app/almuerzafacil/ui/theme/Order.kt
@@ -1,7 +1,42 @@
 package ghoststudios.app.almuerzafacil.ui.theme
 
+import java.util.Date
+
 class Order(
     val id : String? = null,
     val idClient : String? = null,
-    val listOfLunch: ArrayList<Lunch>
-)
+    val lunches: MutableMap<String, Int> = mutableMapOf(),
+    val dateOrdered: Date,
+    val dateToDeliver:Date,
+    var total: Int = 0,
+    var wasDelivered : Boolean
+){
+    fun addLunch(lunchID: String, cantidad: Int) {
+        lunches[lunchID] = (lunches[lunchID] ?: 0) + cantidad
+        calculateTotal()
+    }
+
+    fun deleteLunch(lunchID: String, cantidad: Int) {
+        val currentCantidad = lunches[lunchID] ?: 0
+        if (currentCantidad > cantidad) {
+            lunches[lunchID] = currentCantidad - cantidad
+        } else {
+            lunches.remove(lunchID)
+        }
+        calculateTotal()
+    }
+
+    fun calculateTotal() {
+
+    }
+
+    fun setOrderAsDelivered() {
+
+        if (lunches.isNotEmpty()) {
+            wasDelivered = true
+            println("Pedido confirmado: $id")
+        } else {
+            println("El pedido no tiene almuerzos agregados.")
+        }
+    }
+}
